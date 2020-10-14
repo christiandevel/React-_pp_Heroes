d21900e2e76c6246ae288e75884f86d8aaeff54d
ChristianCeballos <christianmoreno531@gmail.com>
2020-10-14 15:46:42 -0500

    Cambios para produccion

---
+++ docs/precache-manifest.8d53f21bc48f0cc9539b4f16a262316d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763e86c02cad0c5fcb2c6695cb784ecd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1618bbd866f1550f356",
+    "url": "/static/js/2.9d900a41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9d900a41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "081cf40fb831eb128c3c",
+    "url": "/static/js/main.a6671c49.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
